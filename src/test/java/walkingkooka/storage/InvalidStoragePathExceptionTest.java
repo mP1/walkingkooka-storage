7a933e20057877711b5cc09529377418e836c7e4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-26 13:39:51 +1000

    https://github.com/mP1/walkingkooka/pull/3039 ThrowableTesting.getMessageAndCheck was checkMessage
    - https://github.com/mP1/walkingkooka/pull/3039
    - ThrowableTesting.getMessageAndCheck was checkMessage

--- src/test/java/walkingkooka/storage/InvalidStoragePathExceptionTest.java
+++ src/test/java/walkingkooka/storage/InvalidStoragePathExceptionTest.java
@@ -42,7 +42,10 @@ public final class InvalidStoragePathExceptionTest implements ThrowableTesting2<
 
         final InvalidStoragePathException thrown = new InvalidStoragePathException(message, path);
 
-        this.checkMessage(thrown, message + " " + '"' + path + '"');
+        this.getMessageAndCheck(
+            thrown,
+            message + " " + '"' + path + '"'
+        );
         this.checkEquals(
             path,
             thrown.path()
@@ -66,7 +69,10 @@ public final class InvalidStoragePathExceptionTest implements ThrowableTesting2<
         final InvalidStoragePathException thrown = new InvalidStoragePathException(message, StoragePath.ROOT)
             .setPath(path);
 
-        this.checkMessage(thrown, message + " " + '"' + path + '"');
+        this.getMessageAndCheck(
+            thrown,
+            message + " " + '"' + path + '"'
+        );
         this.checkEquals(
             path,
             thrown.path()
