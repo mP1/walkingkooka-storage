b437f990304dceb97f84fe406120e99a8098b6e6
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-05-20 15:05:02 +1000

    StorageSharedTreeMapStore.save value path check added
    - Closes https://github.com/mP1/walkingkooka-storage/issues/269
    - StorageShared.save should fail if StoragePath#isValue false

--- src/main/java/walkingkooka/storage/StorageSharedTreeMapStore.java
+++ src/main/java/walkingkooka/storage/StorageSharedTreeMapStore.java
@@ -59,9 +59,12 @@ final class StorageSharedTreeMapStore<C extends StorageContext> extends StorageS
     @Override
     StorageValue save0(final StorageValue value,
                        final C context) {
-        this.saveRootIfNecessary(context);
-
         final StoragePath path = value.path();
+        if(false == path.isValue() && value.value().isPresent()) {
+            throw path.invalidStoragePathException("Invalid path for a value");
+        }
+
+        this.saveRootIfNecessary(context);
 
         final Store<StoragePath, StorageSharedTreeMapStoreValue> store = this.store;
 
