08d3c08c6e9daacef6cbd7fc7025038e315f91fe
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-06-05 17:42:09 +1000

    StorageWatchersEvent

---
+++ src/main/java/walkingkooka/storage/StorageWatchersEvent.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * Copyright 2024 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.storage;
+
+import walkingkooka.ToStringBuilder;
+import walkingkooka.UsesToStringBuilder;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+/**
+ * The event payload used by {@link StorageWatcher}.
+ */
+final class StorageWatchersEvent implements Consumer<StorageWatcher>,
+    UsesToStringBuilder {
+
+    static StorageWatchersEvent with(final StoragePath path,
+                                     final Optional<?> oldValue,
+                                     final Optional<?> newValue) {
+        return new StorageWatchersEvent(
+            Objects.requireNonNull(path, "path"),
+            Objects.requireNonNull(oldValue, "oldValue"),
+            Objects.requireNonNull(newValue, "newValue")
+        );
+    }
+
+    private StorageWatchersEvent(final StoragePath path,
+                                 final Optional<?> oldValue,
+                                 final Optional<?> newValue) {
+        this.path = path;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+    }
+
+    // Consumer<StorageWatcher>.........................................................................................
+
+    @Override
+    public void accept(final StorageWatcher watcher) {
+        watcher.onStorageValueChange(
+            this.path,
+            this.oldValue,
+            this.newValue
+        );
+    }
+
+    private final StoragePath path;
+    private final Optional<?> oldValue;
+    private final Optional<?> newValue;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.buildFrom(this);
+    }
+
+    // UsesToStringBuilder..............................................................................................
+
+    @Override
+    public void buildToString(final ToStringBuilder b) {
+        b.value(this.path);
+        b.value(this.oldValue);
+        b.value(this.newValue);
+    }
+}
