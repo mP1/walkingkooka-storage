c5179513c91b49de1fdf34916da6a546385ba421
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-06-19 13:32:04 +1000

    StorageConverterStorageBinaryToProperties deleted

--- src/main/java/walkingkooka/storage/convert/StorageConverters.java
+++ src/main/java/walkingkooka/storage/convert/StorageConverters.java
@@ -26,13 +26,6 @@ import walkingkooka.reflect.PublicStaticHelper;
 public final class StorageConverters implements PublicStaticHelper {
 
     /**
-     * {@see StorageConverterStorageBinaryToProperties}
-     */
-    public static <C extends StorageConverterContext> Converter<C> storageBinaryToProperties() {
-        return StorageConverterStorageBinaryToProperties.instance();
-    }
-
-    /**
      * {see StorageConverterStorageBinaryToStorageValueExpression}
      */
     public static <C extends StorageConverterContext> Converter<C> storageBinaryToStorageValueExpression() {
