6297ff4c21b0167ae5112e6aac3f32c7a28f67be
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-13 14:38:26 +1000

    StorageConverterContextTest

---
+++ src/test/java/walkingkooka/storage/convert/StorageConverterContextTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.storage.convert;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.net.header.MediaTypeDetector;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.reflect.JavaVisibility;
+
+public final class StorageConverterContextTest implements ClassTesting2<StorageConverterContext> {
+
+    @Test
+    public void testDoesntImplementMediaTypeDetector() {
+        this.checkEquals(
+            false,
+            new FakeStorageConverterContext() instanceof MediaTypeDetector
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<StorageConverterContext> type() {
+        return StorageConverterContext.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
