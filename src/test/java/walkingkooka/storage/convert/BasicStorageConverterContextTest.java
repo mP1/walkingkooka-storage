122044bb0f3c4847266aa7c9162ccabee308a6d1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-15 13:56:53 +1000

    https://github.com/mP1/walkingkooka/pull/3200 BinaryTextContextTesting.BINARY_TEXT_CONTEXT
    - https://github.com/mP1/walkingkooka/pull/3200
    - BinaryTextContextTesting.BINARY_TEXT_CONTEXT

--- src/test/java/walkingkooka/storage/convert/BasicStorageConverterContextTest.java
+++ src/test/java/walkingkooka/storage/convert/BasicStorageConverterContextTest.java
@@ -41,8 +41,6 @@ import walkingkooka.net.header.MediaTypeDetectors;
 import walkingkooka.props.Properties;
 import walkingkooka.storage.FakeHasUserDirectories;
 import walkingkooka.storage.StoragePath;
-import walkingkooka.text.Indentation;
-import walkingkooka.text.TextPrinting;
 import walkingkooka.tree.expression.ExpressionNumberKind;
 import walkingkooka.tree.expression.convert.ExpressionNumberBinaryNumberConverterFunctions;
 import walkingkooka.tree.expression.convert.ExpressionNumberConverterContexts;
@@ -53,7 +51,6 @@ import walkingkooka.tree.json.marshall.JsonNodeMarshallUnmarshallContexts;
 import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContexts;
 
 import java.math.MathContext;
-import java.nio.charset.StandardCharsets;
 import java.text.DateFormatSymbols;
 import java.time.LocalDateTime;
 import java.util.Currency;
@@ -235,10 +232,7 @@ public final class BasicStorageConverterContextTest implements StorageConverterC
                         ',', // valueSeparator
                         Converters.fake(),
                         BinaryNumberConverterFunctions.fake(), // multiplier
-                        TextPrinting.with(
-                            Indentation.SPACES2,
-                            LINE_ENDING
-                        ).setCharset(StandardCharsets.UTF_8),
+                        BINARY_TEXT_CONTEXT,
                         currencyLocaleContext,
                         DateTimeContexts.basic(
                             DateTimeSymbols.fromDateFormatSymbols(
