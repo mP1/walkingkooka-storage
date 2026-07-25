7409259479d7184fc1871e9e000bc965804f2a38
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-25 20:38:58 +1000

    StorageEnvironmentContexts

---
+++ src/main/java/walkingkooka/storage/StorageEnvironmentContexts.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.storage;
+
+import walkingkooka.environment.EnvironmentContext;
+import walkingkooka.reflect.PublicStaticHelper;
+
+/**
+ * A collection of {@link StorageEnvironmentContext}
+ */
+public final class StorageEnvironmentContexts implements PublicStaticHelper {
+
+    /**
+     * {@see StorageEnvironmentContextBasic}
+     */
+    public static StorageEnvironmentContext basic(final EnvironmentContext environmentContext) {
+        return StorageEnvironmentContextBasic.with(environmentContext);
+    }
+
+    /**
+     * Stop creation
+     */
+    private StorageEnvironmentContexts() {
+        throw new UnsupportedOperationException();
+    }
+}
